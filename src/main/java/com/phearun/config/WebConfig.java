bab0fb6af1beb19412788ecb7e37677b44e4def4
phearun168 <phin.phearun2013@gmail.com>
2023-08-10 23:38:38 -0500

    remove test

--- src/main/java/com/phearun/config/WebConfig.java
+++ src/main/java/com/phearun/config/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    //testing
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
